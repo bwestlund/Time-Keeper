e33b27902746711f4dd10dfdf3cabb3666976fa2
bwestlund <b.e.westlund@gmail.com>
2018-04-16 22:20:27 -0500

    Create checkButtons Function
    Create the Function Check Buttons, added the constand numButtons to allow for easier expansion and changes.

--- time_keeper/time_keeper.ino
+++ time_keeper/time_keeper.ino
@@ -3,15 +3,17 @@
  */
 
 // constants won't change. They're used here to set pin numbers:
+const int numButtons = 4;
 const int buttonPin[] = {4,5,6,7};    // the number of the pushbutton pins
 const int ledPin = 13;      // the number of the LED pin
 
 
+
 // Variables will change:
 int ledState = HIGH;         // the current state of the output pin
-int buttonState[4];             // the current reading from the input pins
+int buttonState[numButtons];             // the current reading from the input pins
 int lastButtonState[] = {LOW, LOW, LOW, LOW};   // the previous reading from the input pins
-int reading[4];
+int reading[numButtons];
 
 // the following variables are unsigned longs because the time, measured in
 // milliseconds, will quickly become a bigger number than can be stored in an int.
@@ -19,16 +21,16 @@ unsigned long lastDebounceTime[] = {0,0,0,0};  // the last time the output pin w
 unsigned long debounceDelay[] = {50, 50, 50, 50};    // the debounce time; increase if the output flickers
 
 void setup() {
-  // Sets the 4 input Pins for Switch to Ground
-  for (int i=0; i < 4; i++){
+  // Sets the numButtons input Pins for Switch to Ground
+  for (int i=0; i < numButtons; i++){
     pinMode(buttonPin[i], INPUT_PULLUP);
   }
   pinMode(ledPin, OUTPUT);
 }
 
-void loop() {
+void checkButtons() {
   // put your main code here, to run repeatedly:
-  for (int i=0; i < 4; i++){
+  for (int i=0; i < numButtons; i++){
     reading[i] = digitalRead(buttonPin[i]);
   }
   // check to see if you just pressed the button
@@ -36,13 +38,13 @@ void loop() {
   // since the last press to ignore any noise:
 
   // If the switch changed, due to noise or pressing:
-  for (int i=0; i < 4; i++){
+  for (int i=0; i < numButtons; i++){
     if (reading[i] != lastButtonState[i]) {
       // reset the debouncing timer
       lastDebounceTime[i] = millis();
     }
   }
-  for (int i=0; i < 4; i++){ 
+  for (int i=0; i < numButtons; i++){ 
     if ((millis() - lastDebounceTime[i]) > debounceDelay[i]) {
       // whatever the reading is at, it's been there for longer than the debounce
       // delay, so take it as the actual current state:
@@ -60,9 +62,13 @@ void loop() {
   }
   // set the LED:
   digitalWrite(ledPin, ledState);
-  for (int i=0; i < 4; i++){
+  for (int i=0; i < numButtons; i++){
     // save the reading. Next time through the loop, it'll be the lastButtonState:
     lastButtonState[i] = reading[i];  
   }
   
+}
+
+void loop() {
+  checkButtons();
 }
